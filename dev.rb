3d940f5741233401983f9284ad5309fac37db52a
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2008-09-11 15:35:46 -0400

    todo about fixtures

--- dev.rb
+++ dev.rb
@@ -1,4 +1,5 @@
 module Dev
+  # TODO need to maintain separate fixtures for testing_ui? and !testing_ui?
   def update_fixture_file(timeline)
     File.open(timeline_fixture_path, "w+") { |f| f.puts timeline.to_yaml }
   end
@@ -7,7 +8,8 @@ module Dev
     File.join Dir.pwd, "timeline.yml"
   end
   
-  # return true for certain side effects.
+  # When returning true, disables all API calls and uses fixtures
+  # for testing UI changes and related behaviors.
   def testing_ui?
     # true
   end
